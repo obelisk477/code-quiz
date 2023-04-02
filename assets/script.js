8ccc4e985898741e3922eec41993910b162060f9
obelisk477 <cwbatte2@gmail.com>
2023-04-01 22:48:08 -0400

    Finished basic HTML and styling for intro page

---
+++ assets/script.js
@@ -0,0 +1,14 @@
+
+
+
+var questionText = document.getElementById("question")
+var hideElements = document.getElementsByClassName("hide")
+
+var quiz = function () {
+    for (var i=0; i< hideElements.length; i++) {
+        hideElements[i].style.display = "none"
+    }
+
+}
+
+
