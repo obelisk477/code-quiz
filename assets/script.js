7adef7279e648f81bbdaaf7637cc0d100b48cbd7
obelisk477 <cwbatte2@gmail.com>
2023-04-02 08:30:00 -0400

    Added iterate question function

--- assets/script.js
+++ assets/script.js
@@ -4,6 +4,7 @@
 var questionText = document.getElementById("question")
 var hideElements = document.getElementsByClassName("hide")
 var main = document.getElementsByTagName("main")[0]
+var currentQuestion = 1
 
 var initializeQuiz = function () {
     for (var i=0; i< hideElements.length; i++) {
@@ -15,10 +16,22 @@ var initializeQuiz = function () {
         var possibleAns = document.createElement("button")
         possibleAns.innerText = questions[1][i+1]
         possibleAns.classList.add("answer-button")
+        possibleAns.setAttribute('onclick',`iterateQuestion(${currentQuestion})`)
         main.appendChild(possibleAns)
     }
 
 }
+
+var iterateQuestion = function(currentQuestion) {
+    let nextQuestion = currentQuestion + 1
+    questionText.innerHTML = questions[nextQuestion].question
+    let buttons = document.querySelectorAll(".answer-button")
+    for (var i=0; i < 4; i++) {
+        buttons[i].innerText = questions[nextQuestion][i+1]
+        buttons[i].setAttribute('onclick', `iterateQuestion(${nextQuestion})` )
+    }
+    
+} 
 
 
 
@@ -32,30 +45,30 @@ var questions = {
     },
     2: {
         question: "What's the answer to question 2?",
-        1: "A",
-        2: "B",
-        3: "C",
-        4: "D",
+        1: "E",
+        2: "F",
+        3: "G",
+        4: "H",
     },
     3: {
         question: "What's the answer to question 3?",
-        1: "A",
-        2: "B",
-        3: "C",
-        4: "D",
+        1: "I",
+        2: "J",
+        3: "K",
+        4: "L",
     },
     4: {
         question: "What's the answer to question 4?",
-        1: "A",
-        2: "B",
-        3: "C",
-        4: "D",
+        1: "M",
+        2: "N",
+        3: "O",
+        4: "P",
     },
     5: {
         question: "What's the answer to question 5?",
-        1: "A",
-        2: "B",
-        3: "C",
-        4: "D",
+        1: "Q",
+        2: "R",
+        3: "S",
+        4: "T",
     }
 }
